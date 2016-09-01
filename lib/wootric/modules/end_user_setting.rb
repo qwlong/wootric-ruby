c74fafb304ba865cbe11b26dc840148d2512773a
Lachlan Priest <lachlanpriest@gmail.com>
2016-09-01 15:27:18 +0800

    Remove unnecessary assignment

--- lib/wootric/modules/end_user_setting.rb
+++ lib/wootric/modules/end_user_setting.rb
@@ -3,16 +3,14 @@ class Wootric::Client
 
     def end_user_settings(end_user_id)
       settings = connection.get("end_users/#{end_user_id}/settings")
-      settings_json = JSON.parse(settings.body)
-      settings_json
+      JSON.parse(settings.body)
     end
 
     def update_user_settings(end_user_id, nps_email)
       new_settings = connection.put("end_users/#{end_user_id}/settings") do |req|
         req.params['email_nps'] = nps_email unless nps_email.nil?
       end
-      new_settings_json = JSON.parse(new_settings.body)
-      new_settings_json
+      JSON.parse(new_settings.body)
     end
 
   end
